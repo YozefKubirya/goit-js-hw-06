a1b573c63320b96e9957d54d85230c11078137cf
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-28 18:10:40 +0300

    correct task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categories = document.querySelectorAll('.item');
 console.log(categories)
 console.log(`Number of categories: ${categories.length} `)
 categories.forEach((categorie) => {
-   const categoriesName = categorie.querySelector('h2').textContent;
-   const categoriesCount=categorie.querySelectorAll('li').length
+   const categoriesName = categorie.firstElementChild.textContent;
+   const categoriesCount=categorie.lastElementChild.children.length
    console.log(`Categorie:${categoriesName},Elements:${categoriesCount}`)
 });
