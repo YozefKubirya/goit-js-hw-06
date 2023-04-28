b9e08043ca0178a9b9359c7ba44177ab7f39c8e6
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-28 19:03:19 +0300

    correct task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,14 @@
 
 const buttonColorPicker = document.querySelector('.change-color');
 const colorValue = document.querySelector('.color');
+
 buttonColorPicker.addEventListener('click', onChangeBodyColor)
 
 function onChangeBodyColor() {
-   document.body.style.backgroundColor = getRandomHexColor();
-   colorValue.textContent = getRandomHexColor();
+  
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = document.body.style.backgroundColor;
+    
 }
 
 function getRandomHexColor() {
